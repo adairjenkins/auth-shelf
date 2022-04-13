77310b810dba8637d43ef9c122205e1dea3a6833
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-13 16:04:15 -0500

    add post route

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -4,13 +4,14 @@ import {useDispatch} from 'react-redux';
 function ShelfForm() {
   
     const [newItem, setNewItem] = useState('');
+    const [newPicture, setNewPicture] = useState('');
 
     const dispatch = useDispatch();
 
     const addItem = (event) => {
         event.preventDefault();
-
-        console.log('CLICK', newItem);
+        dispatch({type: 'ADD_ITEM', payload: {description: newItem, image_url: newPicture }})
+        // console.log('Add', newItem);
         setNewItem('');
     }
 
@@ -18,10 +19,16 @@ function ShelfForm() {
         <>
             <form onSubmit={addItem}>
                 <input 
-                placeholder="Item Name" 
+                placeholder="Item Description" 
                 type="text"
                 value={newItem}
                 onChange={(event) => setNewItem(event.target.value)}
+                />
+                <input 
+                placeholder="Picture URL" 
+                type="text"
+                value={newPicture}
+                onChange={(event) => setNewPicture(event.target.value)}
                 />
                 <button type="submit">Add Item</button>
             </form>
